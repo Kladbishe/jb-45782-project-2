4fb8231b735c64375fad0a6c5d2e662904feb1fb
Pavel <lyubimov99@gmail.com>
2025-09-10 12:06:59 +0300

    finally

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let tokens = [];
 (async () => {
 
     const API_KEY = '1a2c8d09357737c35737275f39a842e96da059232b2d8263157a286df0b80c3f'
-    const CACHE_AGE_IN_SECONDS = 5000000 //5 only
+    const CACHE_AGE_IN_SECONDS = 5 //5 only
 
 
     const getData = async (url, apiKey) => {
@@ -281,5 +281,5 @@ let tokens = [];
     updateLiveChart()
 
     setInterval(updateLiveChart, 5000);//5k
-
+//
 })();
